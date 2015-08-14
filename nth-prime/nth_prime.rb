a81f83dee76d10e1930c7e9707e17d202574f7c5
Tinckle Trivedi <tinckle.trivedi@infibeam.net>
2015-08-14 13:11:14 +0530

    Nth Prime

---
+++ nth-prime/nth_prime.rb
@@ -0,0 +1,23 @@
+class Prime
+  def nth(num)
+    return raise(ArgumentError) if num <= 0
+    return 2 if num == 1
+    
+    prime_array = [2]
+
+    number = 3
+
+    while prime_array.length != num
+      flag = true
+      prime_array.each { |val|  
+        if number % val == 0
+          flag = false
+          number = number.next()
+          break         
+        end
+      }
+      prime_array.push(number) if flag
+    end
+    prime_array.last
+  end
+end
