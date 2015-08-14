4b007ba1c6d98a459bac32efbaa5a524bf09fd08
Tinckle Trivedi <tinckle.trivedi@infibeam.net>
2015-08-14 13:54:19 +0530

    Leap

---
+++ leap/leap.rb
@@ -0,0 +1,19 @@
+module Year
+
+  module_function
+  
+  def leap?(check_year)
+
+    if check_year % 4 == 0 
+      if check_year % 100 == 0
+        if check_year % 400 == 0
+          'Yes, #{check_year} is a leap year'
+        end
+      else
+        'Yes, #{check_year} is a leap year'
+      end
+    end
+    
+  end 
+
+end
