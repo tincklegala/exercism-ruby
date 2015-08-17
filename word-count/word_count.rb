7b7c03c0c6d03a2a6f8830167d9b1e98f7142e4a
Tinckle Trivedi <tinckle.trivedi@infibeam.net>
2015-08-17 14:59:37 +0530

    Word Count

--- word-count/word_count.rb
+++ word-count/word_count.rb
@@ -1,13 +1,15 @@
 class Phrase
-  
+  VERSION = 1
+
   def initialize(phrase)
-    @phrase_check = phrase.split(" ")
+    @phrase = phrase.split(/[ ,\n]/)
   end
 
   def word_count
     count = Hash.new 0
-    @phrase_check.each do |word|
-      cleaned_string = word.gsub(/[^a-zA-Z\-]/,"").downcase
+
+    @phrase.each do |word|
+      cleaned_string = word.gsub(/[^a-zA-Z0-9'?]/,"").downcase
       count[cleaned_string] += 1 unless cleaned_string.empty?
     end
     count
