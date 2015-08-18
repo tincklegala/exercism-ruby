5525fede0793b4f994451f69a1b6bf13e5b42811
Tinckle Trivedi <tinckle.trivedi@infibeam.net>
2015-08-18 15:03:21 +0530

    Difference of squares third iteration

--- difference-of-squares/difference_of_squares.rb
+++ difference-of-squares/difference_of_squares.rb
@@ -4,16 +4,15 @@ class Squares
 	end
 
 	def sum_of_squares
-		(1..@num).inject(0) { |sum, val| sum += val**2 }
+		(1..@num).map { |n| n**2 }.inject(:+)
 	end
 
 	def square_of_sums
-		total = (1..@num).inject(0) { |sum, val|  sum += val }
-		total**2	
+		(1..@num).inject(:+)**2
 	end
 
 	def difference
-		square_of_sums-sum_of_squares
+		square_of_sums - sum_of_squares
 	end
 
 end
