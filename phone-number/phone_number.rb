1246b290b3de9a2cf162ba68426486885c81a51b
Tinckle Trivedi <tinckle.trivedi@infibeam.net>
2015-08-20 13:42:46 +0530

    Phone Number

---
+++ phone-number/phone_number.rb
@@ -0,0 +1,31 @@
+# Validating a phone number
+class PhoneNumber
+  def initialize(phone_number)
+    @phone_number = phone_number
+  end
+
+  def number
+    return '0000000000' unless /[a-zA-Z]/.match(@phone_number).nil?
+
+    cleaned_number = @phone_number.gsub(/[^0-9]/, '')
+    number_size = cleaned_number.size
+    return '0000000000' if number_size < 10 || number_size > 11
+
+    if number_size == 10
+      return cleaned_number
+    elsif cleaned_number[0] == '1'
+      return cleaned_number[1..10]
+    else
+      return '0000000000'
+    end
+  end
+
+  def area_code
+    @phone_number[0, 3]
+  end
+
+  def to_s
+    @phone_number = @phone_number[1, 10] if @phone_number.size == 11
+    '(' + @phone_number[0, 3] + ') ' + @phone_number[3, 3] + '-' + @phone_number[6, 4]
+  end
+end
