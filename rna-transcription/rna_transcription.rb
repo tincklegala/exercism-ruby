39818d21b0bd4161c78fd6080659616c6951bc7a
Tinckle Trivedi <tinckle.trivedi@infibeam.net>
2015-08-20 16:56:43 +0530

    Fifth Iteration

--- rna-transcription/rna_transcription.rb
+++ rna-transcription/rna_transcription.rb
@@ -1,6 +1,6 @@
 # Finding Complements of DNA and RNA strands
 class Complement
-  VERSION = 3
+  VERSION = 5
   RNA_COMPLEMENT = {
     'G' => 'C',
     'C' => 'G',
@@ -10,14 +10,16 @@ class Complement
   DNA_COMPLEMENT = RNA_COMPLEMENT.invert
   
   def self.of_rna(rna_strand)
-    rna_strand.each_char.map do |nucleotide| 
-      (RNA_COMPLEMENT[nucleotide].nil?) ? raise(ArgumentError) : RNA_COMPLEMENT[nucleotide]
-    end.join('')
+    compute_complement(rna_strand, RNA_COMPLEMENT)
   end
 
   def self.of_dna(dna_strand)
-    dna_strand.each_char.map do |nucleotide|
-      (DNA_COMPLEMENT[nucleotide].nil?) ? raise(ArgumentError) : DNA_COMPLEMENT[nucleotide]
+    compute_complement(dna_strand, DNA_COMPLEMENT)
+  end
+
+  def self.compute_complement(strand, complement_hash)
+    strand.each_char.map do |nucleotide|
+      (complement_hash[nucleotide].nil?) ? raise(ArgumentError) : complement_hash[nucleotide]
     end.join('')
   end
 end
