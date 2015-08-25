c03373926721facefa1e309391fe54c21dbc39f5
Tinckle Trivedi <tinckle.trivedi@infibeam.net>
2015-08-25 10:52:30 +0530

    Anagram

--- anagram/anagram.rb
+++ anagram/anagram.rb
@@ -5,19 +5,28 @@ class Anagram
   end
 
   def match(option_list)
-    temp = eql_length(option_list)
-    temp.inject([]) { |a, e| (match_chars?(e.downcase)) ? a.push(e) : a }
+    eql_length = option_list.find_eql_length_words(@word)
+    eql_length.inject([]) { |a, e| anagram?(e.downcase) ? a.push(e) : a }
   end
 
-  def eql_length(option_list)
-    option_list.find_all { |value| value.size == @word.size }
+  def anagram?(option)
+    return true if @word != option && match_word?(option)
   end
 
-  def match_chars?(option)
-    return false if @word == option
+  def match_word?(option)
     option.each_char do |chr|
-      return false unless (@word.include? chr) && (option.count chr) == (@word.count chr)
+      return false unless match_chars?(chr, option.count(chr))
     end
-    true
+  end
+
+  def match_chars?(char, count)
+    @word.include?(char) && @word.count(char) == count
+  end
+end
+
+# Array methods
+class Array
+  def find_eql_length_words(word)
+    find_all { |value| value.size == word.size }
   end
 end
